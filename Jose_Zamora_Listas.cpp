ee7faa7ad0f56fb3396072337352ec5ba1f99b88
Jose David Zamora JIménez <josedzamoraj02@gmail.com>
2022-11-11 23:29:09 -0600

    Examen Estructuras de datos v1.1 - Listas

--- Jose_Zamora_Listas.cpp
+++ Jose_Zamora_Listas.cpp
@@ -1,7 +1,9 @@
 // Librerias de c
+#include <iostream>
 #include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
+using namespace std;
 // Estructura de almacenamiento para los datos
 struct espacio
 {
@@ -14,6 +16,8 @@ typedef struct espacio Nodo;
 void insertar_inicio(Nodo **puntero, int entrada);
 void insertar_final(Nodo **puntero, int entrada);
 void leer(Nodo *puntero);
+void par(Nodo*);
+void impar(Nodo*);
 void menu();
 // Asignacion de NULL al puntero
 Nodo *puntero = NULL;
@@ -33,52 +37,51 @@ void menu()
         // Estructura del menu tomado del ejemplo realizado en clase
         printf("\n\n\t\t\tM E N U\n\n");
         printf("\n\t\t1: Insertar elementos");
-        printf("\n\t\t2: Imprimir lista");
+        printf("\n\t\t2: Imprimir puntero");
         printf("\n\t\t3: Calculos");
         printf("\n\t\t4: Salir\n\n");
         printf("\t\tDigite su opcion: ");
         scanf("%d", &seleccion);
         switch (seleccion)
         {
-        case 1:
+        case 1:// Submenu para elegir la posicion al momento de ubicar el elemento
             printf("\n\n\t\t\tS U B   M E N U\n\n");
             printf("\n\t\t1: Insertar al inicio");
             printf("\n\t\t2: Insertar al final");
+            printf("\n\t\tDigite su opcion: ");
             scanf("%d",&sub_seleccion);
             if(sub_seleccion == 1){
                 printf("\n\tDigite un número entero: ");
                 scanf("%d", &numero);
+                // Llamado al metodo de insertar inicio
                 insertar_inicio(&puntero, numero);
             }
             if(sub_seleccion == 2){
                 printf("\n\tDigite un número entero: ");
                 scanf("%d", &numero);
+                // Llamado al metodo de insertar final
                 insertar_final(&puntero, numero);
             }
-            else{
-                printf("\n\t\t\tOpcion desconocida");
-            }
             break;
-        case 2:
+        case 2:// Lectura de todos los nodos
             if (puntero != NULL)
+                // Llamado al metodo de lectura
                 leer(puntero);
             else
-                printf("\n\t\t\tLista vacia");
+                printf("\n\t\t\tpuntero vacia");// Mensaje de error
             break;
 
-        case 3:
+        case 3:// Submenu para elegir si calcular los numeros pares o impares
             printf("\n\n\t\t\tS U B   M E N U\n\n");
             printf("\n\t\t1: Calcular e imprimir la suma de los valores que sean pares y su promedio");
             printf("\n\t\t2: Calcular e imprimir la suma de los valores que sean impares y su promedio");
+            printf("\n\t\tDigite su opcion: ");
             scanf("%d",&sub_seleccion);
             if (sub_seleccion == 1){
-                
+                par(puntero);
             }
             if (sub_seleccion == 2){
-
-            }
-            else{
-                
+                impar(puntero);
             }
             break;
         }
@@ -100,7 +103,7 @@ void insertar_inicio(Nodo **puntero, int numero)
         nuevo->siguiente = *puntero; // si hay nodos lo conecto al primero
         *puntero = nuevo;
     }
-    printf("\tElemento %d insertado al inicio de la lista !", numero);
+    printf("\tElemento %d insertado al inicio de la puntero !", numero);
 }
 
 void insertar_final(Nodo **puntero, int numero)
@@ -118,23 +121,23 @@ void insertar_final(Nodo **puntero, int numero)
     {
         Nodo *p;
         p = *puntero;
-        // ciclo para llegar al final de la lista
+        // ciclo para llegar al final de la puntero
         while (p->siguiente != NULL)
         {
             p = p->siguiente;
         }
         p->siguiente = nuevo; // conecto el último nodo al nuevo
     }
-    printf("\tElemento %d insertado al final de la lista !", numero);
+    printf("\tElemento %d insertado al final de la puntero !", numero);
 }
 
 void leer(Nodo *puntero)
 {
     int dato_variable;
-    printf("\n\t\t LISTA \n");
+    printf("\n\t\t puntero \n");
 
     Nodo *p = puntero;
-    // ciclo para llegar al final de la lista
+    // ciclo para llegar al final de la puntero
     while (p != NULL)
     {
         printf("\t%d ", p->dato);
@@ -142,4 +145,54 @@ void leer(Nodo *puntero)
     }
 
     printf("\n\n");
+}
+
+void par(Nodo *puntero)
+{
+    int suma_par = 0;
+    int contadorPar = 0;
+    float promedio_par = 0;
+    while (puntero != NULL)
+    {
+        if (puntero->dato % 2 == 0)
+        {
+            contadorPar = contadorPar + 1;
+            suma_par = suma_par + puntero->dato;
+            puntero = puntero->siguiente;
+        }
+        else
+        {
+            puntero = puntero->siguiente;
+        }
+
+    }
+    promedio_par = suma_par;
+    promedio_par = (promedio_par / contadorPar);
+    cout << "\n\t\tEl valor de suma es " << suma_par;
+    cout << "\n\t\tY su promedio es " << promedio_par;
+}
+
+void impar(Nodo *puntero)
+{
+    int suma_impar = 0;
+    int contador_impar = 0;
+    float promedio_impar = 0;
+    while (puntero != NULL)
+    {
+        if (puntero->dato % 2 != 0)
+        {
+            contador_impar = contador_impar + 1;
+            suma_impar = suma_impar + puntero->dato;
+            puntero = puntero->siguiente;
+        }
+        else
+        {
+            puntero = puntero->siguiente;
+        }
+
+    }
+    promedio_impar = suma_impar;
+    promedio_impar = (promedio_impar / contador_impar);
+    cout << "\n\t\tEl valor de suma es " << suma_impar;
+    cout << "\n\t\tY su promedio es " << promedio_impar;
 }
